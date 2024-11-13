6bf363773ca9775ee00bef11b1911ce20b902062
swetamaurya <mauryasweta062@gmail.com>
2024-11-13 20:45:04 +0530

    firebase

--- utils/fireBase.js
+++ utils/fireBase.js
@@ -2,9 +2,7 @@ const admin = require('firebase-admin');
 const dotenv = require("dotenv")
 dotenv.config()
  
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-const serviceAccount = require(serviceAccountPath);
-console.log('Service Account Key:', process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
